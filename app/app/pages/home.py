e5b173450e17f726564f5279ea5c17e629325179
root <root@DESKTOP-86SA0R3.>
2025-01-26 02:43:05 -0600

    made changes to home saving them now

--- app/app/pages/home.py
+++ app/app/pages/home.py
@@ -3,26 +3,36 @@ from rxconfig import config
 from ..state import State
 from ..components import navigation_bar
 
-
-
 @rx.page(route="/")
 def home_page() -> rx.Component:
     return rx.container(
         navigation_bar(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+            rx.image(
+                src="https://lp-auto-assets.s3.us-east-1.amazonaws.com/20/Toyota/model-lineups/sedans/header.jpg",  # Path to your image file
+                alt="Toyota Vehicles",  # Alt text for the image
+                width="100%",  # Adjust the width as needed (optional)
             ),
+            rx.link(
+                rx.button("Join the Toyota Family!"),
+                href="http://localhost:3000/models/",
+                is_external=True,
+            ),
+            # Commented out section
+            # rx.heading("Join the Toyota Family!", size="9"),
+            # rx.text(
+            #     "Get started by editing ",
+            #     rx.code(f"{config.app_name}/{config.app_name}.py"),
+            #     size="5",
+            # ),
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
                 is_external=True,
             ),
             spacing="5",
-            justify="center",
+            justify="center",  # Horizontal centering
+            align="center",  # Vertical centering
             min_height="85vh",
         ),
         rx.logo(),
